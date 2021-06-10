ecbdea6951244651d75a1d779df18bd9bd51e38c
Saurabh Singh Gautam <32809271+sauram@users.noreply.github.com>
2021-06-10 09:48:16 +0530

    Update settings.py

--- silentOberverServer/settings.py
+++ silentOberverServer/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6y37@2@g_bi#_t8*6yg(p2+yi3f*n&$m*pco!483j&xy9m@55%'
+SECRET_KEY = '' # has been deleted.
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
